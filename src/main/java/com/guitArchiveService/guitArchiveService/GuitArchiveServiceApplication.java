36f0a9dac76c9a69449adf315384720c09cb6d90
JamieHunstad <jamie.hunstad@gmail.com>
2023-12-17 21:11:10 -0600

    Updated path to heroku frontend

--- src/main/java/com/guitArchiveService/guitArchiveService/GuitArchiveServiceApplication.java
+++ src/main/java/com/guitArchiveService/guitArchiveService/GuitArchiveServiceApplication.java
@@ -2,12 +2,35 @@ package com.guitArchiveService.guitArchiveService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GuitArchiveServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuitArchiveServiceApplication.class, args);
+	}
+	@Configuration
+	public class WebConfig
+	{
+		@Bean
+		public WebMvcConfigurer corsConfigurer()
+		{
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("https://guitararchive-app-3f4567fa06fe.herokuapp.com/")
+							.allowedMethods("GET", "POST", "PUT", "DELETE")
+							.allowedHeaders("*")
+							.allowCredentials(true)
+							.exposedHeaders("Authorization");
+				}
+			};
+		}
 	}
 
 }
